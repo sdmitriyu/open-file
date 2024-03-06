40fcc1fa41198522ba96d34a5bc1bd85ef5dedaa
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-03-06 21:29:30 +0300

    Тема форматирования

---
+++ format.py
@@ -0,0 +1,17 @@
+team1_num = 5
+print('В команде Мастера кода участников: %d!' %(team1_num))
+team1_num = 5
+team2_num = 6
+print('Итого сегодня в командах участников: %d и %d!' %(team1_num, team2_num))
+score_2 = 42
+print('Команда Волшебники данных решила задач: {}!' .format(score_2))
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {} с !' .format(team1_time))
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result = 'Мастера кода'
+print(f'Результат битвы: победа команды {challenge_result}!')
+tasks_total = 82
+time_avg = 350.4
+print(f'Сегодня было решенно {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
